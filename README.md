f2131f9499ee6d37575dbe92016381496bb766d6
PedroFernandez30 <pedrofj1999@gmail.com>
2020-09-30 16:19:33 +0200

    Actualización de Angular

--- README.md
+++ README.md
@@ -1,4 +1,4 @@
-# Angular8
+# angular10
 
 This project was generated with [Angular CLI](https://github.com/angular/angular-cli) version 10.1.3.
 
--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -20,16 +20,16 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have as title 'angular8'`, () => {
+  it(`should have as title 'angular10'`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
-    expect(app.title).toEqual('angular8');
+    expect(app.title).toEqual('angular10');
   });
 
   it('should render title', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.nativeElement;
-    expect(compiled.querySelector('.content span').textContent).toContain('angular8 app is running!');
+    expect(compiled.querySelector('.content span').textContent).toContain('angular10 app is running!');
   });
 });
